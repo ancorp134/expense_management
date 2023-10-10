cea6e214d69f86b0bc63338b348c7bd73d1750ea
Ankit Tyagi <ancorp007@gmail.com>
2023-10-10 15:06:15 +0530

    update profile

--- base/views.py
+++ base/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 # Create your views here.
 User = get_user_model()
-
 @login_required(login_url='login')
 def dashboard(request):
     try:
